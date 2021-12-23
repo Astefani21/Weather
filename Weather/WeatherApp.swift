1582d9b5fe89f8b298a5b77f3c3bcc5d5f53463c
Andy Stefani <astefani21@gmail.com>
2021-12-23 10:30:54 -0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alexis Willey on 12/23/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
